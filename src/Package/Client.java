14b8f143c8cd1e5b1a8413f753a5fc0ad71b6bef
ergophile-geek <hawanatamang@gmail.com>
2023-07-10 01:15:28 +0545

    Code added

--- src/Package/Client.java
+++ src/Package/Client.java
@@ -8,86 +8,86 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client implements ActionListener {
-    Server server = new Server();
     static JFrame frame2 = new JFrame("Client");
+    static JLabel lbl1 = new JLabel();
     static JLabel lbl2 = new JLabel();
     static JLabel heading = new JLabel();
     static JTextField tf2 = new JTextField();
-    static JTextField tf4 = new JTextField();
-    static JButton btn = new JButton("Connect");
+    static JTextArea tf4 = new JTextArea();
     static JButton btn2 = new JButton();
-    public void layout(){
+    static String sendingMessage;
+    static String receivingMessage;
+    static PrintWriter printWriter;
+
+    public void layout() {
 
         int a = 800;
         int b = 200;
 
-        frame2.setSize(600,500);
+        frame2.setSize(500, 500);
         frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame2.setLocation(a,b);
-        frame2.setVisible(true);
+        frame2.setLocation(a, b);
         frame2.setLayout(null);
 
-        heading.setBounds(10,10,500,40);
-        heading.setFont(new Font("Arial",Font.BOLD,30));
+        heading.setBounds(10, 10, 300, 40);
+        heading.setFont(new Font("Arial", Font.BOLD, 30));
         heading.setText("CHAT APPLICATION");
         frame2.add(heading);
 
-        lbl2.setBounds(10,400,50,30);
+        lbl1.setBounds(350, 10, 200, 40);
+        frame2.add(lbl1);
+
+        lbl2.setBounds(10, 400, 50, 30);
         lbl2.setText("Client: ");
         frame2.add(lbl2);
 
-        tf2.setBounds(60,400,300,30);
+        tf2.setBounds(60, 400, 300, 30);
         frame2.add(tf2);
 
-        tf4.setBounds(20,60,450,300);
+        tf4.setBounds(20, 60, 450, 300);
+        tf4.setEditable(false);  // Prevent manual editing
         frame2.add(tf4);
-
-        btn.setBounds(500,10,100,40);
-        btn.setBackground(Color.BLUE);
-        btn.setForeground(Color.WHITE);
-        frame2.add(btn);
-
 
         btn2.setBackground(Color.GRAY);
         btn2.setForeground(Color.WHITE);
-        btn2.setBounds(380,400,80,30);
+        btn2.setBounds(380, 400, 80, 30);
         btn2.setText("Send");
         frame2.add(btn2);
         btn2.addActionListener(this);
 
+        frame2.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btn){
-            try {
-                Socket socket = new Socket("127.0.0.1", 12345);
-                System.out.println("A server is connected");
-                BufferedReader socketData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String userInput = tf2.getText();
-                OutputStream outputStream = socket.getOutputStream();
-                PrintWriter printWriter = new PrintWriter(outputStream,true);
-                String sendingMessage, receivingMessage;
-                if(e.getSource() == btn2) {
-                    while (true) {
-                        receivingMessage = socketData.readLine();
-                        tf4.setText("Server: " + receivingMessage);
-                        sendingMessage = userInput;
-                        printWriter.println(sendingMessage);
-                        tf4.setText("\nClient: " + sendingMessage);
-                        printWriter.flush();
-                    }
-                }
-            }catch(IOException e2 ){
-                e2.printStackTrace();
-            }
+        if (e.getSource() == btn2) {
+            sendingMessage = tf2.getText();
+            System.out.println("Server is connected");
+            tf4.setText(sendingMessage);
+            printWriter.println(sendingMessage);
+            tf2.setText("");  // Clear the input text field after sending
         }
     }
-
 
     public static void main(String[] args) {
         Client obj2 = new Client();
         obj2.layout();
 
+        try {
+            Socket socket = new Socket("127.0.0.1", 12345);
+            lbl1.setText("Server is connected");
+
+            BufferedReader socketData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            OutputStream outputStream = socket.getOutputStream();
+             printWriter = new PrintWriter(outputStream, true);
+
+            while (true) {
+                receivingMessage = socketData.readLine();
+                tf4.setText(receivingMessage);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Package/Server.java
+++ src/Package/Server.java
@@ -10,85 +10,85 @@ import java.net.Socket;
 
 public class Server implements ActionListener {
     static JFrame frame = new JFrame("Server");
-    static  JLabel lbl = new JLabel();
+    static JLabel lbl = new JLabel();
+    static JLabel lbl2 = new JLabel();
     static JLabel heading = new JLabel();
-    static JTextField tf = new JTextField();
-    static JTextField tf3 = new JTextField();
+    static JTextArea tf = new JTextArea();
+    static JTextArea tf3 = new JTextArea();
     static JButton btn = new JButton();
-    static JButton btn2 = new JButton("Connect");
-    public void layout(){
+    static String sendingMessage;
+    static String receivingMessage;
+    static PrintWriter printWriter;
+
+    public void layout() {
         int x = 100;
         int y = 200;
 
-        frame.setSize(600,500);
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocation(x,y);
-        frame.setVisible(true);
+        frame.setLocation(x, y);
         frame.setLayout(null);
 
-        heading.setBounds(10,10,500,40);
-        heading.setFont(new Font("Arial",Font.BOLD,30));
+        heading.setBounds(10, 10, 300, 40);
+        heading.setFont(new Font("Arial", Font.BOLD, 30));
         heading.setText("CHAT APPLICATION");
         frame.add(heading);
 
-        btn2.setBounds(450,10,100,40);
-        btn2.setBackground(Color.BLUE);
-        btn2.setForeground(Color.WHITE);
-        frame.add(btn2);
+        lbl2.setBounds(350,10,200,40);
+        frame.add(lbl2);
 
-        lbl.setBounds(10,400,50,30);
+        lbl.setBounds(10, 400, 50, 30);
         lbl.setText("Server: ");
         frame.add(lbl);
 
-        tf.setBounds(60,400,300,30);
+        tf.setBounds(60, 400, 300, 30);
+        tf3.setEditable(false);
         frame.add(tf);
 
-        tf3.setBounds(20,60,450,300);
+        tf3.setBounds(20, 60, 450, 300);
         frame.add(tf3);
 
         btn.setBackground(Color.GRAY);
         btn.setForeground(Color.WHITE);
-        btn.setBounds(380,400,80,30);
+        btn.setBounds(380, 400, 80, 30);
         btn.setText("Send");
         frame.add(btn);
         btn.addActionListener(this);
 
-
+        frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btn2)
-        {
-            try{
-                ServerSocket ss = new ServerSocket(12345);
-                Socket socket = ss.accept();
-                System.out.println("A client is connected");
-                BufferedReader socketData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String userInput = tf.getText();
-                OutputStream outputStream = socket.getOutputStream();
-                PrintWriter printWriter = new PrintWriter(outputStream,true);
-                String sendingMessage, receivingMessage;
-                if(e.getSource() == btn) {
-                    while (true) {
-                        sendingMessage = userInput;
-                        printWriter.println(sendingMessage);
-                        tf3.setText("Server: " + sendingMessage);
-
-                        printWriter.flush();
-                        receivingMessage = socketData.readLine();
-                        tf3.setText("Client: " + receivingMessage);
-                    }
-                }
-            }catch(IOException e2){
-                e2.printStackTrace();
-            }
+        if (e.getSource() == btn) {
+            sendingMessage = tf.getText();
+            System.out.println(sendingMessage);
+            tf3.setText(sendingMessage);
+            printWriter.println(sendingMessage);
+            tf.setText("");  // Clear the input text area after sending
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Server obj = new Server();
         obj.layout();
 
+        try {
+            ServerSocket ss = new ServerSocket(12345);
+            Socket socket = ss.accept();
+            lbl2.setText("Client is connected");
+
+            BufferedReader socketData = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            OutputStream outputStream = socket.getOutputStream();
+            printWriter = new PrintWriter(outputStream, true);
+
+            while (true) {
+
+                receivingMessage = socketData.readLine();
+                tf3.setText(receivingMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
